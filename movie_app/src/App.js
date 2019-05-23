7f91a9323361dc2f8e978ea55fd301cb3d493875
김혜원 <terroir7@buyis.co.kr>
2019-05-23 14:56:08 +0900

    finish

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -4,8 +4,7 @@ import Movie from './Movie';
 
 class App extends Component {
   // Render : componentWillMount() => render() => coponentDidMound()
-  // Update : componentWillReceiveProps() => shouldComponentUpdate() == true 
-  //  => componentWillUpdate() => render() => componentDidUpdate()
+  // Update : componentWillReceiveProps() => shouldComponentUpdate() == true => componentWillUpdate() => render() => componentDidUpdate()
 
   state = {}
 
@@ -14,8 +13,14 @@ class App extends Component {
   }
 
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.large_cover_image} key={index} />
+    const movies = this.state.movies.map(movie => {
+      return <Movie 
+        title={movie.title}
+        poster={movie.medium_cover_image}
+        key={movie.id}
+        genres={movie.genres}
+        synopsis={movie.synopsis}
+      />
     })
     return movies;
   }
@@ -23,21 +28,22 @@ class App extends Component {
   _getMovies = async () => {
     const movies = await this._callApi(); // _callApi()가 끝나기를 기다리는 것(성공여부 상관X)
     this.setState({
-      movies: movies
+      movies
     })
   }
 
   _callApi = () => {
-    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count')
     .then(response => response.json())
     .then(json => json.data.movies)
     .catch(err => console.log(err))
   }
 
   render() {
+    const { movies } = this.state;
     return (
-      <div className="App">
-        {this.state.movies ? this._renderMovies() : 'Loading..'}
+      <div className={movies ? "App" : "App--loading"}>
+        {movies ? this._renderMovies() : 'Loading..'}
       </div>
     )
   }
--- movie_app/src/Movie.js
+++ movie_app/src/Movie.js
@@ -1,29 +1,59 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import LineEllipsis from 'react-lines-ellipsis';
 import './Movie.css';
 
-function Movie({title, poster}) {
+function Movie({title, poster, genres, synopsis}) {
     return (
-        <div>
-            <MoviePoster poster={poster} />
-            <h1>{title}</h1>
+        <div className="Movie">
+            <div className="Movie__Column">
+                <MoviePoster poster={poster} alt={title} />
+            </div>
+            <div className="Movie__Column">
+                <h1>{title}</h1>
+                <div className="Movie__Genres">
+                    {genres.map((genre, index) => <MovieGenre genre={genre} key={index} />)}
+                </div>
+                <div className="Movie__Synopsis">
+                    <LineEllipsis 
+                        text={synopsis}
+                        maxLine='3'
+                        ellipsis='...'
+                        trimRight
+                        basedOn='letters'
+                    />
+                </div>
+            </div>
         </div>
     )
 }
 
-function MoviePoster({poster}) { // functional 컴포넌트 = Dump 컴포넌트 (state가 없다.)
+function MoviePoster({poster, alt}) { // functional 컴포넌트 = Dump 컴포넌트 (state가 없다.)
     return (
-        <img src={poster} alt="Movie Poster" />
+        <img src={poster} alt={alt} title={alt} className="Movie__Poster" />
+    )
+}
+
+function MovieGenre({genre}) {
+    return (
+        <span className="Movie__Genre">{genre}</span>
     )
 }
 
 Movie.propTypes = {
     title: PropTypes.string.isRequired,
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.array.isRequired,
+    synopsis: PropTypes.string.isRequired
 }
 
 MoviePoster.propTypes = {
-    poster: PropTypes.string.isRequired
+    poster: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired
+}
+
+MovieGenre.propTypes = {
+    genre: PropTypes.string.isRequired
 }
 
 export default Movie;
