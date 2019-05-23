53b4b5d19296f2148a84de7653c03a07dbd50c4d
김혜원 <terroir7@buyis.co.kr>
2019-05-23 15:29:15 +0900

    Updates

---
+++ precache-manifest.6f233a1d5de8c69f4d6434a2d4424da6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "084bfa76ae22c7457a57ad70e1b6a816",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "0a410947e366e4383af4",
+    "url": "/movie_app/static/css/main.9b2995fb.chunk.css"
+  },
+  {
+    "revision": "5cdb4062c6dfdd077298",
+    "url": "/movie_app/static/js/2.31ebf22a.chunk.js"
+  },
+  {
+    "revision": "0a410947e366e4383af4",
+    "url": "/movie_app/static/js/main.a28a76de.chunk.js"
+  },
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  }
+]);
